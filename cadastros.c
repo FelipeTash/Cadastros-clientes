a481f17632b5ea45ca3a917d18d000e49192927c
Felipe da Mata <felipenv01.764@gmail.com>
2025-08-24 13:54:45 -0300

    Update and rename ver.c to cadastros.c

--- cadastros.c
+++ cadastros.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,25 +15,39 @@ int main(void) {
     struct Cliente clientes[100];
     int opcao, numClientes = 0;
     char input[100];
+    
+    FILE *logs;
+    logs = fopen("logs.txt", "a");
+
+    if (logs == NULL) {
+        printf("Erro ao abrir o ficheiro.\n");
+        return 1;
+    }
 
     do {
-        printf("Selecione uma opÁ„o:\n");
+        printf("Selecione uma op√ß√£o:\n");
         printf("1 - Adicionar Cliente\n");
         printf("2 - Parar o programa\n\n");
 
-        printf("> "); fgets(input, sizeof(input), stdin);
+        printf("> ");
+        
+        fgets(input, sizeof(input), stdin);
         input[strcspn(input, "\n")] = '\0';
         opcao = (int)strtol(input, NULL, 10);
 
         switch (opcao) {
             case 1: {
-                printf("Nome:\n"); fgets(clientes[numClientes].nome, sizeof(clientes[numClientes].nome), stdin);
-
-                printf("Nome: %s\n", clientes[numClientes].nome);
+                printf("Nome:\n"); 
+                fgets(clientes[numClientes].nome, sizeof(clientes[numClientes].nome), stdin);
                 clientes[numClientes].nome[strcspn(clientes[numClientes].nome, "\n")] = '\0';
+                
+                printf("Nome: %s\n", clientes[numClientes].nome);
                 numClientes++;
                 printf("Numero de clientes (Atual): %i\n", numClientes);
-
+                
+                fprintf(logs, "Nome: %s\nNumero de clientes: %i\n", clientes[numClientes-1].nome, numClientes);
+                fflush(logs); // For√ßa a escrita no arquivo
+                
                 break;
             }
 
@@ -42,5 +57,7 @@ int main(void) {
             }
         }
     } while(opcao != 2);
+    
+    fclose(logs); // Fecha o arquivo no final
     return 0;
 }
